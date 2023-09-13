885596a271d4648af7d382bf4d80bb657f704b07
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-13 10:43:41 -0300

    DECLARAÇÃO DE VARIÁVEIS TIPOS PUBLICA E PRIVADA

--- M09A04/src/cursojava/main/Inicio.java
+++ M09A04/src/cursojava/main/Inicio.java
@@ -7,7 +7,7 @@ public class Inicio {
 	public static void main(String[] args) {
 		
 		
-		/**/            
+		/* instancia*/            
 		Aluno aluno = new Aluno();
 		aluno.nome = "Paulo"; // setando um atributo publico
 		aluno.setIdade(33);  //  setrando um atributo privado
