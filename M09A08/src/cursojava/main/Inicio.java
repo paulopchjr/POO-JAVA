7a083db9135a159b42bb801eb8c5be082db781db
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-13 10:17:28 -0300

    SOLUCIONANDO O PROBLEMA DE NULLPOINTEREXCEPTION

--- M09A08/src/cursojava/main/Inicio.java
+++ M09A08/src/cursojava/main/Inicio.java
@@ -6,18 +6,24 @@ public class Inicio {
 
 	public static void main(String[] args) {
 
-		Aluno aluno = null; /*
-							 * Aqui é o erro ( java.lang.NullPointerException), toda referencia tem que ter
-							 * uma instancia de um objeto
-							 */
+		/*
+		 * Aluno aluno = null; * Aqui é o erro ( java.lang.NullPointerException), toda
+		 * referencia tem que ter uma instancia de um objeto
+		 * 
+		 * 
+		 * aluno.setNome("Paulo"); aluno.setNota1(10); aluno.setNota2(10);
+		 * aluno.setNota3(7); aluno.setNota4(10);
+		 * System.out.println(aluno.ImprimeResultado());
+		 */
 
-		aluno.setNome("Paulo");
-		aluno.setNota1(10);
-		aluno.setNota2(10);
-		aluno.setNota3(7);
-		aluno.setNota4(10);
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Paulo Cezar");
+		aluno2.setNota1(10);
+		aluno2.setNota2(10);
+		aluno2.setNota3(7);
+		aluno2.setNota4(10);
 
-		System.out.println(aluno.ImprimeResultado());
+		System.out.println(aluno2.ImprimeResultado());
 
 	}
 }
