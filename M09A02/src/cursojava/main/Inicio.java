830bcce713380d40948edddd3bfc9c580cd73c46
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-13 10:47:21 -0300

    SOBRE O CONSTRUTOR DO OBJETO

--- M09A02/src/cursojava/main/Inicio.java
+++ M09A02/src/cursojava/main/Inicio.java
@@ -10,17 +10,17 @@ public class Inicio {
 		Aluno aluno = new Aluno();
 
 		// setando os atributos do aluno
-		aluno.setNome("Paulo Cezar");
-		aluno.setIdade(33);
+		aluno.setNome("Paulo Cezar Henrique Junior");
+		aluno.setIdade(31);
 
 		
-		System.out.println("Nome do Aluno 1 = " + aluno.getNome());
+		System.out.println("Nome do Aluno 1 = " + aluno.getNome() + "| idade Aluno 1 = " + aluno.getIdade());
 		
 		/////////////// ***////////
 		
 		/* Segundo jeito de instancia o aluno com o nome no construtor*/
 		Aluno aluno2 = new Aluno("Junior",33); // ja seta o atributos dentro da instancia;
-		System.out.println("Nome do Aluno 2 = " + aluno2.getNome() + "\n idade  = " + aluno2.getIdade());
+		System.out.println("Nome do Aluno 2 = " + aluno2.getNome() + "| idade aluno 2  = " + aluno2.getIdade());
 		
 		
 		
