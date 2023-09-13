99c3e1ecf4aad539a18f4b09db2480b18550af0d
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-13 10:38:20 -0300

    INSTANCIAÇÃO, ATRIBUTOS E REFERÊNCIAS

--- M09A03/src/cursojava/main/Inicio.java
+++ M09A03/src/cursojava/main/Inicio.java
@@ -7,7 +7,7 @@ public class Inicio {
 	public static void main(String[] args) {
 		
 		
-		/* new Aluno() -> é uma instância ( Criação de um Objeto)
+		/* new Aluno() --> é uma instância ( Criação de um Objeto)
 		 * aluno é uma referência para o objeto/instancia Aluno
 		 * 
 		 * */            
