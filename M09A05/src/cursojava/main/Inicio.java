9ebf8e7c7e4332847adcced90dfbf7662854c145
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-13 10:34:47 -0300

    MÉTODOS GETTERS AND SETTERS

--- M09A05/src/cursojava/main/Inicio.java
+++ M09A05/src/cursojava/main/Inicio.java
@@ -22,6 +22,21 @@ public class Inicio {
 		
 		System.out.println(carro);
 		
+		System.out.println("\n");
+		
+		Carro carro2 = new Carro();
+		carro2.setNome("CRUZE");
+		carro2.setMarca("CHEVROLLET");
+		carro2.setCor("CHUMBO");
+		carro2.setModelo("LTZ");
+		carro2.setAno(2016);
+		carro2.setValor(64.00);
+		carro2.setDescricao(" Multimídia com tela de 10,1 polegadas.\n Volante com teclas touch.\n Parachoque resedenhados. \n Rodas Aro 18 poleagdas" +
+		"\n Motor: 2.0 TSI EA888 -> 231 cv e 35,7 kgfm. \n Velocidade: 0 a 100 em 6,7 segundos velocidade maxima 249, km/h");
+		
+		
+		System.out.println(carro2);
+		
 		
 		
 	}
