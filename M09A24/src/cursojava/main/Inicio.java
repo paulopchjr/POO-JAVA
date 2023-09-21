2d61e3801d063fa212a9422005d83964dd99a8b5
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-21 12:48:45 -0300

    SUBSTITUINDO UM ALUNO NA LISTA

--- M09A24/src/cursojava/main/Inicio.java
+++ M09A24/src/cursojava/main/Inicio.java
@@ -73,22 +73,71 @@ public class Inicio {
 
 			}
 
-			
-			
 			/* Percorrendo a lista de alunos */
+
+			System.out.println("Lista de Alunos:");
 			for (int posicao = 0; posicao < alunos.size(); posicao++) {
 				/* pegando o aluno pela sua posicao da lista */
 				Aluno aluno = alunos.get(posicao);
-				
-				
 
 				System.out.println(aluno.getDadosAluno());
-				
-				for(int posDisciplina =0; posDisciplina < aluno.getDisciplinas().size();posDisciplina++) {
+
+				for (int posDisciplina = 0; posDisciplina < aluno.getDisciplinas().size(); posDisciplina++) {
 					Disciplina dis = aluno.getDisciplinas().get(posDisciplina);
-					System.out.println("Disciplina = "+ dis.getDisciplina() + " | Nota :" + dis.getNota());
+					System.out.println("Disciplina = " + dis.getDisciplina() + " | Nota :" + dis.getNota());
 				}
-				
+
+				System.out.println("*****************************");
+
+			}
+
+			int acao2 = JOptionPane.showConfirmDialog(null, "Deseja trocar o Aluno?");
+
+			while (acao2 == 0) {
+				String alunoTroca = JOptionPane.showInputDialog("Qual é aluno você deseja trocar?");
+				for (int posiAluno = 0; posiAluno < alunos.size(); posiAluno++) {
+					Aluno aluno = alunos.get(posiAluno);
+
+					if (aluno.getNome().equalsIgnoreCase(alunoTroca)) {
+						Aluno trocarAluno = new Aluno();
+						String nomeAlunoTroca = JOptionPane.showInputDialog("Qual é o nome do novo aluno ?");
+						trocarAluno.setNome(nomeAlunoTroca);
+						String raAlunoTroca = JOptionPane.showInputDialog("Qual é o ra do aluno " + trocarAluno.getNome() + " ? ");
+						trocarAluno.setRa(raAlunoTroca);
+						for (int disciNova = 1; disciNova <= 4; disciNova++) {
+							Disciplina novaDisciplina = new Disciplina();
+							String novaD = JOptionPane.showInputDialog("Qual é a" + disciNova
+									+ " disciplina nova do aluno" + trocarAluno.getNome() + " ?");
+							novaDisciplina.setDisciplina(novaD);
+							String novaN = JOptionPane.showInputDialog("Qual é o valor da nota" + disciNova
+									+ " disciplina nova do aluno" + trocarAluno.getNome() + " ?");
+
+							novaDisciplina.setNota(Double.parseDouble(novaN));
+
+							trocarAluno.getDisciplinas().add(novaDisciplina);
+
+						}
+						// aqui atualiza a lista
+						alunos.set(posiAluno, trocarAluno);
+
+					}
+
+				}
+				acao2 = JOptionPane.showConfirmDialog(null, "Continuar a trocar aluno?");
+			}
+
+			System.out.println("Lista Atualizada de Alunos:");
+			for (int p = 0; p < alunos.size(); p++) {
+				/* pegando o aluno pela sua posicao da lista */
+				Aluno aluno = alunos.get(p);
+
+				System.out.println(aluno.getDadosAluno());
+
+				for (int posDisciplina = 0; posDisciplina < aluno.getDisciplinas().size(); posDisciplina++) {
+					Disciplina dis = aluno.getDisciplinas().get(posDisciplina);
+					System.out.println("Disciplina = " + dis.getDisciplina() + " | Nota :" + dis.getNota());
+				}
+
 				System.out.println("*****************************");
 
 			}
