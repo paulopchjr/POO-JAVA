38b02258d68ab100619eb4f241657703fbeb6634
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-09-20 22:58:06 -0300

    PROCURANDO E REMOVENDO UM ALUNO DA LISTA

---
+++ M09A22/src/cursojava/main/Inicio.java
@@ -0,0 +1,96 @@
+package cursojava.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		int acao1 = JOptionPane.showConfirmDialog(null, "Deseja cadastrar aluno?");
+
+		if (acao1 == 0) {
+
+			String qtdAluno = JOptionPane.showInputDialog("Quantos alunos deseja cadastrar ?");
+
+			int totalAluno = Integer.parseInt(qtdAluno);
+
+			List<Aluno> alunos = new ArrayList<Aluno>();
+
+			for (int i = 1; i <= totalAluno; i++) {
+
+				Aluno aluno = new Aluno();
+				String nomeAluno = JOptionPane.showInputDialog("Qual é o nome do " + i + " aluno ?");
+				String raAluno = JOptionPane.showInputDialog("Qual é o RA do " + i + " aluno ?");
+
+				aluno.setNome(nomeAluno);
+				aluno.setRa(raAluno);
+
+				for (int d = 1; d <= 4; d++) {
+					Disciplina disciplinas = new Disciplina();
+
+					String nomeDisciplina = JOptionPane.showInputDialog("Qual é o nome da Disciplina ?" + d);
+					disciplinas.setDisciplina(nomeDisciplina);
+					String notaDisciplina = JOptionPane
+							.showInputDialog("Qual é a nota da Disciplina " + disciplinas.getDisciplina());
+					disciplinas.setNota(Double.parseDouble(notaDisciplina));
+
+					/* Adicionando as discplinas no aluno */
+					aluno.getDisciplinas().add(disciplinas);
+
+				}
+
+				int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+				if (escolha == 0) {
+					int continuarremover = 0;
+					int posicao = 1;
+					while (continuarremover == 0) {
+						String disciplinaRemover = JOptionPane.showInputDialog("Qual é a disciplina 1,2, 3, 4 ?");
+
+						// removendo a posicao da lista;
+						aluno.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+						posicao++; /* Soma uma posicao */
+						continuarremover = JOptionPane.showConfirmDialog(null,
+								"Deseja remover mais alguma disciplina?");
+					}
+				}
+
+				alunos.add(aluno);
+			}
+
+			int acao2 = JOptionPane.showConfirmDialog(null, "Deseja remover um Aluno no sistema?");
+
+			if (acao2 == 0) {
+				String nomeAlunoProcurado = JOptionPane.showInputDialog("Qual é o nome do aluno que deseja remover?");
+				for (Aluno aluno : alunos) {
+					if (aluno.getNome().equalsIgnoreCase(nomeAlunoProcurado)) {
+
+						alunos.remove(aluno); // removendo aluno
+						System.out.println("Aluno, " + nomeAlunoProcurado + " foi excluido da lista");
+						
+						break;
+					} else {
+						aluno.getNome();
+					}
+				}
+
+			} else {
+
+				// caso a acao for nao ==1 , ou cancela ==2 imprime o relatorio todo
+				for (Aluno aluno : alunos) {
+					System.out.println(aluno.getDadosAluno());
+				}
+			}
+
+		} else {
+
+			JOptionPane.showInternalMessageDialog(null, "Saindo do sistema");
+		}
+
+	}
+}
